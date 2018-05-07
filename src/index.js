d86dae5d598465ec15de26f52cc6c6294a0eadac
nami4628 <nami4628@cafe24.com>
2018-05-07 13:10:29 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'styles/main.scss';
 import Root from './Root';
 import registerServiceWorker from './registerServiceWorker';
 import { AppContainer } from 'react-hot-loader';
